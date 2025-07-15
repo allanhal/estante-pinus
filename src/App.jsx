b2cbd983e353c9bbab636445d8355d8f3db4c1b8
Allan Araújo <allan.hal@gmail.com>
2025-07-15 13:52:52 -0300

    adicionado query param

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
+import { useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-// import * as THREE from "three";
-// import SceneInit from "./lib/SceneInit";
 import Shelf3D from "./Shelf3D";
 import Controls from "./Controls";
 
@@ -16,7 +15,6 @@ const TIRAS_POR_PRATELEIRA = 7;
 const LARGURA_MINIMA = 30;
 const LARGURA_MAXIMA = 90;
 
-// const ALTURA_MINIMA = 30;
 const ALTURA_MAXIMA = 250;
 
 const PROFUNDIDADE_MINIMA = 20;
@@ -41,14 +39,58 @@ function App() {
   );
   const [price, setPrice] = useState();
 
+  const [searchParams] = useSearchParams();
+  const [queryParams, setQueryParams] = useState({});
+
+  useEffect(() => {
+    const entries = Object.fromEntries(searchParams.entries());
+    setQueryParams(entries);
+  }, [searchParams]);
+
+  useEffect(() => {
+    const {
+      altura,
+      largura,
+      profundidade,
+      ripas_por_prateleira,
+      espaco_entre_prateleiras,
+    } = queryParams;
+
+    // http://localhost:3000/?altura=80&largura=80&profundidade=30&ripas_por_prateleira=7&espaco_entre_prateleiras=20&name=allan&age=36
+
+    if (altura === undefined) {
+      setHeight(ALTURA);
+    } else {
+      setHeight(parseInt(altura, 10));
+    }
+    if (largura === undefined) {
+      setWidth(LARGURA);
+    } else {
+      setWidth(parseInt(largura, 10));
+    }
+    if (profundidade === undefined) {
+      setDepth(PROFUNDIDADE);
+    } else {
+      setDepth(parseInt(profundidade, 10));
+    }
+    if (ripas_por_prateleira === undefined) {
+      setSlatsPerShelf(TIRAS_POR_PRATELEIRA);
+    } else {
+      setSlatsPerShelf(parseInt(ripas_por_prateleira, 10));
+    }
+    if (espaco_entre_prateleiras === undefined) {
+      setSpacePerShelf(DISTANCIA_ENTRE_PRATELEIRAS);
+    } else {
+      setSpacePerShelf(parseInt(espaco_entre_prateleiras, 10));
+    }
+  }, [queryParams]);
+
   return (
     <div className="grid grid-cols-5 h-screen">
-      <div
-        // className="col-span-5 m-5 flex items-center justify-center border border-gray-300 rounded-xl shadow-lg"
-        className="col-span-5 m-5 flex items-center justify-center"
-      >
-        <p className="text-[100px] dancing-script text-amber-600">Estante Pinus</p>
-        {/* <p className="text-[100px] dancing-script text-sky-950 text-shadow-2xs text-shadow-sky-300">Estante Pinus</p> */}
+      <div className="col-span-5 m-5 flex items-center justify-center">
+        <p className="text-[100px] dancing-script text-amber-600">
+          Estante Pinus
+        </p>
       </div>
       <div
         id="scene-wrapper"
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
